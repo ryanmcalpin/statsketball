eb1b8e93673b3e18bce860009bcaab0748801c96
Mark <mark.aaron.fisher@gmail.com>
2017-05-15 16:46:00 -0700

    began working on breadcrumbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  currentPage;
+
+  constructor(){}
+
+  ngOnInit() {
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
+      this.currentPage = event['url'].split('/').filter(route => (route));
+      console.log(this.currentPage);
+    })
+  }
 }
