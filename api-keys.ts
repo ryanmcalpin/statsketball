7d1c617fdc29e2337327ef6657be8ffdc4ddbbb9
Janek Brandt and Ryan McAlpin <janek@>
2017-05-16 09:56:56 -0700

    remove api-keys file

--- api-keys.ts
+++
@@ -1,7 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyCjhmB_-kapof8ocW-LcsTeWFnPM-T7_R4",
-  authDomain: "statsketball-27ff5.firebaseapp.com",
-  databaseURL: "https://statsketball-27ff5.firebaseio.com",
-  storageBucket: "statsketball-27ff5.appspot.com",
-  messagingSenderId: "109040503794"
-};
