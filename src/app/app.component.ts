8f4123ec0b44eee7eaaef425a3e9bd5f79b6e291
Mark <mark.aaron.fisher@gmail.com>
2017-05-15 17:42:39 -0700

    successfully capture currentPage sub urls and add basketball icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component ,OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { DbService } from './db.service';
+import { MaterializeAction } from 'angular2-materialize';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/takeUntil';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   currentPage;
 
-  constructor(){}
+  constructor(private router: Router){}
 
   ngOnInit() {
     this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
+      console.log(event['url']);
       this.currentPage = event['url'].split('/').filter(route => (route));
+      console.log("Current page is: ")
       console.log(this.currentPage);
     })
   }
